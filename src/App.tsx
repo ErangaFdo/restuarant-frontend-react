fc6b3c45673c090ff3c26b3a16b07995ec44590a
ErangaFernando404 <erangafdo1994@gmail.com>
2025-12-30 07:31:22 +0530

    create frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { lazy } from "react"
+
+const Services = lazy(() => import("./pages/Services"))
+const About = lazy(() => import("./pages/About"))
+const Home = lazy(() => import("./pages/Home"))
+const Login = lazy(() => import("./pages/Login"))
+const Register = lazy(() => import("./pages/Register"))
+
+
 
 function App() {
 
---
+++ src/pages/Register.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Rejister() {
+  return (
+    <div>Rejister</div>
+  )
+}
+
+export default Rejister
